eb1eb8d3cc5e1a21ed5ec917290cd11c690cb644
geovanaAnani <geovana.ananias04@gmail.com>
2022-02-24 15:04:43 -0300

    Exercícios de Loop

--- Aula03/src/main/kotlin/Exercício06 (DO).kt
+++ Aula03/src/main/kotlin/Exercício06 (DO).kt
@@ -2,8 +2,8 @@ fun main (){
 
 
          var mult3 =0
-         var media =0
-         var soma =0
+         var media =0.0
+         var soma =0.0
          var num=0
 
     do {
@@ -13,12 +13,12 @@ fun main (){
         if (num %3 == 0 && num != 0 ){
             soma += num
             mult3 ++
-            media = soma / mult3
+
         }
 
     }while (num != 0)
 
-
+    media = soma / mult3
 
     println("Dos números digitados, a média dos números multiplos de 3 é: $media")
 }
